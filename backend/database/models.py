5b759d1ffe469cc5d3d9ad7eba16cba882dfa1d6
kevincolahan <kevin.colahan@gmail.com>
2026-01-02 12:49:30 -0500

    Update models.py

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Float, Boolean, ForeignKey, JSON, Text, Enum
+from sqlalchemy import Column, Integer, String, DateTime, Date, Float, Boolean, ForeignKey, JSON, Text, Enum
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from datetime import datetime
